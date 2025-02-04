b49a13eaf5c147d9e4ba5d32bd7a1baae4ad8062
LelleOne <103145061+LelleOne@users.noreply.github.com>
2025-02-04 09:14:28 +0100

    first init

---
+++ src/getKey.js
@@ -0,0 +1,46 @@
+const axios = require('axios');
+
+async function getKey() {
+  try {
+    const response = await axios.post(
+      "https://web.skola24.se/api/get/timetable/render/key",
+      {},
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "X-Scope": "8a22163c-8662-4535-9050-bc5e1923df48",
+        },
+      },
+    );
+    console.log(response.data.data.key);
+    return response.data.data.key;
+  } catch (error) {
+    console.error("Error in getKey: ", error);
+    throw error;
+  }
+}
+
+getKey();
+
+
+// async function getKey(){
+//   try{
+//     const response = await fetch(
+//       "https://web.skola24.se/api/encrypt/signature",
+//       {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//           "X-Scope": "8a22163c-8662-4535-9050-bc5e1923df48",
+//         },
+//         body: null,
+//       },
+//     );
+//     console.log("key " + response.data.data.key)
+//     return response.data.data.key;
+//   }catch(error){
+//     console.error("Error in getKey:", error);
+//     throw error;
+//   }
+// }
+
